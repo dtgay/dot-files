9012cd837f41d2ad04f3b56e2e1a110cd7ed0fd9
David Gay <oddshocks@riseup.net>
2013-08-18 17:15:36 -0400

    Update bin files and add .gtkrc-2.0.

---
+++ bin/fix_tahrir.sh
@@ -0,0 +1,4 @@
+pip uninstall tw2.bootstrap.forms
+pip install git+git://github.com/toscawidgets/tw2.bootstrap.git
+pip uninstall tahrir-api
+pip install git+git://github.com/fedora-infra/tahrir-api.git
